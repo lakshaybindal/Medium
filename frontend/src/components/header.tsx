0c39bd641fd133dc00986c0d556f7c463982b447
Lakshay-Bindal <lakshaybindal999@gmail.com>
2025-02-03 22:59:29 +0530

    bug fix

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,7 +1,11 @@
-]
 import { Link } from "react-router-dom";
 
-export default function Header(props: { heading: string; subheading: string; log: string; link: string }) {
+export default function Header(props: {
+  heading: string;
+  subheading: string;
+  log: string;
+  link: string;
+}) {
   return (
     <div className="text-center mb-6">
       <h1 className="text-3xl font-bold">{props.heading}</h1>
